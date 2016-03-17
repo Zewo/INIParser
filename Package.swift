045767570bedcbd84069a69b921ce437e6ea4ac4
badim <goloveychuk@mail.ru>
2016-03-17 21:18:05 +0200

    fix package

--- Package.swift
+++ Package.swift
@@ -4,5 +4,6 @@ let package = Package(
     name: "INIParser",
           dependencies: [
             .Package(url: "https://github.com/Zewo/File.git", majorVersion: 0, minor: 2),
+            .Package(url: "https://github.com/Zewo/CLibvenice.git", majorVersion: 0, minor: 2),
     ]
 )
